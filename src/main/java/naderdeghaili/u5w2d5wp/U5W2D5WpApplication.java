c8d3b263984fc49aa0c9446e442a82ad7535d22d
NAD3R <naderdeghaili@gmail.com>
2026-02-13 09:49:06 +0100

    initial commit. env.properties hidden and ignored

---
+++ src/main/java/naderdeghaili/u5w2d5wp/U5W2D5WpApplication.java
@@ -0,0 +1,13 @@
+package naderdeghaili.u5w2d5wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W2D5WpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(U5W2D5WpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/naderdeghaili/u5w2d5wp/U5W2D5WpApplicationTests.java
@@ -0,0 +1,13 @@
+package naderdeghaili.u5w2d5wp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W2D5WpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
